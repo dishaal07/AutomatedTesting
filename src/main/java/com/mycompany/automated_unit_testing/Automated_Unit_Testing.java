f665cbfee89a33d8699eaaefeda822974878248f
dishaal07 <166614465+dishaal07@users.noreply.github.com>
2024-05-17 14:15:56 +0200

    Add files via upload

---
+++ src/main/java/com/mycompany/automated_unit_testing/Automated_Unit_Testing.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.automated_unit_testing;
+
+/**
+ *
+ * @author disha
+ */
+public class Automated_Unit_Testing {
+
+    public static void main(String[] args) {
+        /*
+        Automated unit testing using github actions --> Create a workflow
+        - Complete all unit Tests 
+        -Modify the pom.xml file of your program
+        - Create a Workflow 
+        - Note : The JDK version included in your workflow may change 
+                 depending on the version of java you have used for your program 
+        */
+    } 
+}
